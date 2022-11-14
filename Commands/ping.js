8e0d66e9f7fc2583a43fbd99856f6ed73ecb1cd3
Stephen Onochie <stephenconochie@gmail.com>
2022-11-13 21:27:13 -0500

    Added Say Command to repeat what ever someone says

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,5 +1,4 @@
 module.exports.run = (client, message, args) => {
-    console.log("The channel is " + message.channel);
     message.channel.send("pong!");
   }
 
---
+++ Commands/say.js
@@ -0,0 +1,5 @@
+module.exports.run = (client, message, args) => {
+        let toSay = args.join(" ");
+        if(!toSay) return message.channel.send({content: "You have to provide something!"});
+        message.channel.send({content: toSay});
+}
