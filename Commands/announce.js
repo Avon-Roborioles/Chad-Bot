2b2b97ffdea22eb63f0dcf3d2446e4fe6d1aa4fc
Stephen-Onochie <stephenconochie@gmail.com>
2022-11-15 19:12:00 -0800

    Slight Change that might fix announcement command

--- Commands/announce.js
+++ Commands/announce.js
@@ -2,5 +2,5 @@ module.exports.run = (client, message, args) => {
 const annouceChannel = 1006721419826176020;
 let announcement = args.join(" ");
 if(!toSay) return message.channel.send({content: "You have to provide something!"});
-  client.channels.get(announceChannel).send(announcement);
+  message.channels.get(announceChannel).send(announcement);
 }
