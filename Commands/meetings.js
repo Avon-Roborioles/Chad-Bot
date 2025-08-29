23e824863b75294df57313f2077fd98bb8e5a9f8
TornadoBoah <nboekekitt@gmail.com>
2025-08-29 09:39:30 -0400

    8/31 to 9/6 Meetings

--- Commands/meetings.js
+++ Commands/meetings.js
@@ -6,13 +6,13 @@ module.exports.run = (client, message, args) => {
   const { EmbedBuilder } = require('discord.js');
 
   //Notes
-  let Title = '8/24 to 8/30 Meetings:';
+  let Title = '8/31 to 9/6 Meetings:';
   let TuesdayNotes = '- No Meetings';
-  let WednesdayNotes = '- Applications and Schoology tasks due';
-  let ThursdayNotes = '- No Meetings';
-  let FridayNotes = '- Team List Posted outside of E111 & L119';
-  let SaturdayNotes = '- No Meetings';
-  let MoreNotes = "Applications and Schoology Assignments will be due no later then Wednesday, August, 27 @ 3:10 PM ";
+  let WednesdayNotes = '- No Meetings';
+  let ThursdayNotes = '- First Meeting - 6 to 8pm';
+  let FridayNotes = '- No Meetings';
+  let SaturdayNotes = '- FTC Decode Kickoff - 11am to 1pm';
+  let MoreNotes = "Kickoff will be in the auditorium. Enter at Door 38 ";
 
 
   // inside a command, event listener, etc.
@@ -24,10 +24,10 @@ module.exports.run = (client, message, args) => {
     //.setDescription('All Meetings for the Week:')
     .addFields(
       // { name: 'Tuesday', value: TuesdayNotes },
-      { name: 'Wednesday', value: WednesdayNotes},
-      // { name: 'Thursday', value: ThursdayNotes, inline: false },
-      { name: 'Friday', value: FridayNotes },
-      // { name: 'Saturday', value: SaturdayNotes },
+      // { name: 'Wednesday', value: WednesdayNotes},
+      { name: 'Thursday', value: ThursdayNotes },
+      // { name: 'Friday', value: FridayNotes },
+      { name: 'Saturday', value: SaturdayNotes },
       // { name: '\u200B', value: '\u200B' },
       // { name: 'All Week:', value: 'No meetings! Make sure to get your Applications turned in and the Schoology Tasks Completed' },
       { name: 'Additional Notes', value: MoreNotes },
