5217a4865b482e65543d43e08a146cb4b9a36e4a
TornadoBoah <nboekekitt@gmail.com>
2025-10-27 14:33:00 -0400

    10/26 to 11/1 Meetings

--- Commands/meetings.js
+++ Commands/meetings.js
@@ -6,13 +6,13 @@ module.exports.run = (client, message, args) => {
   const { EmbedBuilder } = require('discord.js');
 
   //Notes
-  let Title = '10/19 to 10/25 Meetings:';
-  let TuesdayNotes = '- No Meetings';
-  let WednesdayNotes = '- No Meetings';
-  let ThursdayNotes = '- No Meetings';
+  let Title = '10/26 to 11/1 Meetings:';
+  let TuesdayNotes = '- Meeting - 6 to 8pm';
+  let WednesdayNotes = '- Trunk or Treat 5:30 to 8pm';
+  let ThursdayNotes = '- Meeting - 6 to 8pm';
   let FridayNotes = '- No Meetings';
   let SaturdayNotes = '- Meeting - 9am to 12pm';
-  let MoreNotes = "Enjoy Your Fall Break :)";
+  let MoreNotes = "Get your dues turned in!";
 
 
   // inside a command, event listener, etc.
@@ -23,9 +23,9 @@ module.exports.run = (client, message, args) => {
     .setAuthor({ name: 'Chad Bot | Roborioles', iconURL: 'https://asset-cdn.schoology.com/system/files/imagecache/profile_reg/courselogos/logo-1923125308_632db8558d3f9.png?1663940693', url: 'https://www.instagram.com/ahs.roborioles/' })
     //.setDescription('All Meetings for the Week:')
     .addFields(
-      // { name: 'Tuesday', value: TuesdayNotes },
-      // { name: 'Wednesday', value: WednesdayNotes},
-      // { name: 'Thursday', value: ThursdayNotes },
+      { name: 'Tuesday', value: TuesdayNotes },
+      { name: 'Wednesday', value: WednesdayNotes},
+      { name: 'Thursday', value: ThursdayNotes },
       // { name: 'Friday', value: FridayNotes },
       { name: 'Saturday', value: SaturdayNotes },
       // { name: '\u200B', value: '\u200B' },
